5db03843ce874545cef256324132f612abd1661b
im-sudhir <ssudhir794@gmail.com>
2025-05-04 00:28:50 +0530

    added custom manager for published posts

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -18,6 +22,9 @@ class Post(models.Model):
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
     status= models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
+
+    objects=models.Manager()
+    published=PublishedManager()
 
     class Meta:
         ordering=['-publish']
