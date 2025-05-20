f3a65dba6f6da8da53f17fd4e8c195e365c56f09
im-sudhir <ssudhir794@gmail.com>
2025-05-20 19:18:29 +0530

    added full text search functionality in the project

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 # Create your views here.
 
@@ -86,7 +87,10 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query=form.cleaned_data['query']
-            results=Post.published.annotate(search=SearchVector('title','body'),).filter(search=query)
+            # search_vector=SearchVector('title', weight='A')+SearchVector('body',weight='B')
+            # search_query=SearchQuery(query)
+            # results=Post.published.annotate(search=search_vector,rank=SearchRank(search_vector,search_query)).filter(rank__gte=0.3).order_by('-rank')
+            results=Post.published.annotate(similarity=TrigramSimilarity('title',query),).filter(similarity__gt=0.1).order_by('-similarity')
         
     return render(request,'blog/post/search.html',
                       {'form':form, 'query':query,'results':results})
